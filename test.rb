5109cce8b2d8be76da489ffa8a736af44c385824
litswd0211 <[hiromu.kariya@gmail.com]>
2021-09-27 22:25:14 +0900

    test

--- test.rb
+++ test.rb
@@ -1,13 +1,12 @@
-N, M = gets.split.map &:to_i
-pipes = M.times.map {gets; gets.split.map &:to_i}
-checks = []
-while pipe = pipes[-1]
-    color = pipe.shift or next pipes.pop
-    if checks[color]
-        pipes << checks[color]
-        checks[color] = nil
-    else
-        checks[color] = pipes.pop
-    end
+N = gets.to_i
+a = gets.chomp.split.map(&:to_i)
+
+puts a.join(" ")
+
+(1..N-1).each do |i|
+  ai = a.delete_at(i)
+  newi = (0..i-1).to_a.reverse.find {|j| a[j] < ai }
+  newi = newi ? newi + 1 : 0
+  a.insert(newi, ai)
+  puts a.join(" ")
 end
-puts checks.any?{_1} ? :No : :Yes
