ecc6573666b748845213524390022d00f32f3886
litswd0211 <hiromu.kariya@gmail.com>
2021-10-18 00:49:05 +0900

    cpp実行環境追加

---
+++ README.md
@@ -0,0 +1,12 @@
+## Ruby
+
+```
+ruby ./test.rb < input.tx
+```
+
+## C++
+
+```
+g++ main.cpp -std=c++14 -I ../ac-library/
+./a.out < input.txt 
+```
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+
+int main()
+{
+  int i;
+  cin >> i;
+  cout << i * 2 << endl;
+}
