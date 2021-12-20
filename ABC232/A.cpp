ce83e54fff5e8bb5a51e3ae67cedf00d9b1ddf25
litswd0211 <hiromu.kariya@gmail.com>
2021-12-20 23:04:47 +0900

    12/20 提出分

---
+++ ABC232/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+typedef long long ll;
+const int inf = INT_MAX / 2;
+const ll infl = 1LL << 60;
+
+int main() {
+  string s;
+  cin >> s;
+  int a = (int)(s[0] - '0');
+  int b = (int)(s[2] - '0');
+  cout << a * b << endl;
+
+  return 0;
+}
